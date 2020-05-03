b40bdc685e7c1e238f0617f21302c8332ed1b7b5
Paul Brunache <paulbrunache@gmail.com>
2020-05-03 00:36:38 -0400

    working implementation with asset pipeline

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,7 +3,6 @@ const WebpackAssetsManifest = require('webpack-assets-manifest');
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  publicPath: isDev ? '':'dist/',
   filenameHashing: false,
   productionSourceMap: false,
   devServer: {
@@ -35,12 +34,25 @@ module.exports = {
     // this path is specific to my project
     config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
 
+    //change the name of image outputs for manifest
     config.module
       .rule('images')
       .use('url-loader')
       .tap(options => {
-        // options.fallback.options.name =  '../dist/[name].[hash:8].[ext]';
+        options.fallback.options.name =  'images/[name].[ext]';
         return options;
       });
+
+    config.plugin('copy').tap(([options]) => {
+      // copy images to the rails public directory
+      // you need this because the images aren't recognized during the transition
+      // so i'm adding them to the public folder
+      options.push({
+        from: path.resolve(__dirname, 'src/assets'),
+        to: path.resolve(__dirname, '../public/images'),
+        toType: 'dir',
+      })
+      return [options]
+    })
   },
 };
